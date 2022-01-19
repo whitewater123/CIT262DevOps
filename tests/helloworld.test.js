971ed5b30b7ca271e273138e3a6452937c38660d
whitewater123 <derekwashburn@hotmail.com>
2022-01-19 16:37:04 -0700

    Small bug fixes
    Small bug fixes

--- tests/helloworld.test.js
+++
@@ -1,8 +0,0 @@
-import {expect, it} from "@jest/globals";
-import sayHello from "../ultils/helloworld.js";
-
-it("Should say hello", ()=>{
-    const sayHello = sayHello();
-
-    expect(sayHello).toBe("hello");
-});
