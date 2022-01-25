2d4a3c2bed4074e696384240d88bc0602b3013de
whitewater123 <derekwashburn@hotmail.com>
2022-01-25 10:59:52 -0700

    Solved error from merging
    Solved error from merging

--- test/helloworld.test.js
+++ test/helloworld.test.js
@@ -1,8 +1,8 @@
-import sayHello from "../utils/helloworld.mjs";
+/*import sayHello from "../utils/helloworld.mjs";
 import assert from 'assert';
 
 it("Hello World", ()=>{
     const hello = sayHello();
 
     assert.equal(hello,"hello");
-});
+});*/
